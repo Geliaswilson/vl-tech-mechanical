74b399cc62df3925cad0be80e726a382ad5ab26d
geliaswilson <geliaswilson@gmail.com>
2024-10-17 13:31:58 -0400

    install react-router-dom, install sass, import browserrouter,routes,route into app.jsx, build browserrouter, routes, route code and make route path "/" to homepage page in app.jsx; create homepagehero component and build basic html and styling, make partials files for breakpoints and variables only incl. variables for tablet and desktop breakpoints, and mixins for tablet and desktop responsiveness; link homepagehero component to hompage page

---
+++ src/components/HomepageHero.jsx
@@ -0,0 +1,16 @@
+import "./HomepageHero.scss";
+const HomepageHero = () => {
+  return (
+    <>
+      <section className="hero">
+        <div className="hero__text">
+          <h2>VL Mechanical</h2>
+          <p>24/7 Services</p>
+          <p>Plumbing, Sewage</p>
+          <button className="hero__button">Free Quote</button>
+        </div>
+      </section>
+    </>
+  );
+};
+export default HomepageHero;
