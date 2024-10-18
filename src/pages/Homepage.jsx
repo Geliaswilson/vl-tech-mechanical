02355faeb32a1d6c2575c5e16bddef9c6a6a63ce
geliaswilson <geliaswilson@gmail.com>
2024-10-17 21:59:31 -0400

    fix imports

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,10 @@
 import HomepageHero from "../components/HomepageHero";
-import Header from "../components/Header/Header";
+
 const Homepage = () => {
     return (
         <>
         <main>
-        <Header />
+        
         <HomepageHero />
         </main>
         </>
