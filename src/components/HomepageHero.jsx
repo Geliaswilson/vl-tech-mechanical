89b2714af95a886d56577df2b1fe35b6cba25f3e
geliaswilson <geliaswilson@gmail.com>
2024-10-17 21:14:12 -0400

    import ./app.scss into app.jsx, apply @use "./styles/partials/global" as * to app.scss, move hero-image.jpeg into assets folder,  apply classNames to h2 and 2 p tags in HomepageHero.jsx, Build upon preliminary styling of hero section, re-add min breakpoint variabls to _variables.scss file

--- src/components/HomepageHero.jsx
+++ src/components/HomepageHero.jsx
@@ -4,9 +4,9 @@ const HomepageHero = () => {
     <>
       <section className="hero">
         <div className="hero__text">
-          <h2>VL Mechanical</h2>
-          <p>24/7 Services</p>
-          <p>Plumbing, Sewage</p>
+          <h2 className="hero__company">VL Mechanical</h2>
+          <p className="hero__time">24/7 Services</p>
+          <p className="hero__services">Plumbing, Sewage</p>
           <button className="hero__button">Free Quote</button>
         </div>
       </section>
