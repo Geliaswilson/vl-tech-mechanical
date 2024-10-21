7d1c74d39614316d7559b427e946cc2dca843334
Ralfi <ri564@nyu.edu>
2024-10-21 18:01:49 -0400

    Add the form page and its associated styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Homepage from "./pages/Homepage";
+import FormPage from "./pages/Formpage";
 import Header from "./components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import WhatWeDo from "./Components/WhatWeDo/WhatWeDo";
@@ -11,6 +12,7 @@ function App() {
     <Header />
      <Routes>
       <Route path="/" element={<Homepage />}/>
+      <Route path="/Form" element={<FormPage />} />
      </Routes>
      <Footer />
      </BrowserRouter>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,43 @@
-import "./Header.scss"
+import { useNavigate } from "react-router-dom";
+import "./Header.scss";
+
 function Header() {
-    return(
-        <header className="header">
-            <img className="header__img" src="src/assets/icons/Screenshot 2024-10-17 at 6.45.34 PM.png" alt="" />
-            <nav className="header__nav">
-                <ul className="header__nav-ul">
-                    <li className="header__nav-li"><a className="header__link" href="#">Home</a></li>
-                    <li className="header__nav-li"><a className="header__link" href="#">Services</a></li>
-                    <li className="header__nav-li"><a className="header__link" href="#">About</a></li>
-                </ul>
-            </nav>
-            <button className="header__button">Free Quote Today!</button>
-        </header>
-    )
+  const navigate = useNavigate();
+
+  return (
+    <header className="header">
+      <img
+        className="header__img"
+        src="src/assets/icons/Screenshot 2024-10-17 at 6.45.34 PM.png"
+        alt="Logo"
+      />
+      <nav className="header__nav">
+        <ul className="header__nav-ul">
+          <li className="header__nav-li">
+            <a className="header__link" href="#">
+              Home
+            </a>
+          </li>
+          <li className="header__nav-li">
+            <a className="header__link" href="#">
+              Services
+            </a>
+          </li>
+          <li className="header__nav-li">
+            <a className="header__link" href="#">
+              About
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <button
+        className="header__button"
+        onClick={() => navigate("/form")}
+      >
+        Free Quote Today!
+      </button>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
