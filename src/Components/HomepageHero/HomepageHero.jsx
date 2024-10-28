c943f0903b0f1c6b1a308ed49112285b416b9885
Ralfi <ri564@nyu.edu>
2024-10-28 12:43:00 -0400

    Add styling to reshape footer, header, hero

--- src/Components/HomepageHero/HomepageHero.jsx
+++ src/Components/HomepageHero/HomepageHero.jsx
@@ -6,7 +6,7 @@ const HomepageHero = () => {
         <div className="hero__text">
           <h2 className="hero__company">VL Tech</h2>
           <p className="hero__time">24/7 Services</p>
-          <p className="hero__services">Plumbing, Sewage</p>
+          <p className="hero__services">Emergency Calls at All times</p>
           <button className="hero__button">Free Quote</button>
         </div>
       </section>
