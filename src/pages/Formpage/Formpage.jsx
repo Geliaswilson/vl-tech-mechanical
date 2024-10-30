275a45c020aff298330e92985d8d957a70577c91
geliaswilson <geliaswilson@gmail.com>
2024-10-30 16:31:38 -0400

    create formpage.scss, apply styling to formpage class

--- src/pages/Formpage/Formpage.jsx
+++ src/pages/Formpage/Formpage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Form from '../../Components/Form/Form';  
 import '../../Components/Form/Form.scss';     
+import "./Formpage.scss"
 
 
 const FormPage = () => {
   return (
     <>
+    <main className="formpage">
       <Form />
+      </main>
     </>
   );
 };
